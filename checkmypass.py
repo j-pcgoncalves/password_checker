c349ff160b11376f33907b70e973f8203ad17852
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-10-08 19:33:43 +0100

    complete request_api_data

--- checkmypass.py
+++ checkmypass.py
@@ -1,10 +1,14 @@
 import requests
-
-url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
-res = requests.get(url)
-
-print(res)
+import hashlib
 
 def request_api_data(query_char):
-    pass
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
 
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again.')
+    
+    return res
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8'))
